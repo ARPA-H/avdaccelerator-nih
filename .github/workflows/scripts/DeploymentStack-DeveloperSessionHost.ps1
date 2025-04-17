e4bf5dc33de02dd8a7482c5ba49cc3496ded6544
John Ireland <41406288+jwaltireland@users.noreply.github.com>
2025-04-17 15:51:32 -0400

    restored to last working commit

--- .github/workflows/scripts/DeploymentStack-DeveloperSessionHost.ps1
+++ .github/workflows/scripts/DeploymentStack-DeveloperSessionHost.ps1
@@ -12,33 +12,24 @@
     [string]$identityDomainName, 
     [string]$avdOuPath,
     [string]$update_existing_stack,
-    [string]$securityPrincipalId
+    [string]$securityPrincipalId,
+    [string]$avdHostPoolType,
+    [int]$avdDeploySessionHostsCount
 )
-
-# $paramAzSubscriptionDeploymentStackDeployment = @{
-#     Name = $DeploymentStackName
-#     Location = $Location
-#     TemplateFile = $TemplateFile
-#     TemplateParameterFile = $ParametersFile
-#     ActionOnUnmanage = "detachAll" 
-#     DenySettingsMode = "none"
-#     avdSessionHostCustomNamePrefix = $avdSessionHostCustomNamePrefix 
-#     deploymentEnvironment = $deploymentEnvironment 
-#     avdWorkloadSubsId = $avdWorkloadSubsId
-#     imageGallerySubscriptionId = $imageGallerySubscriptionId
-#     existingVnetAvdSubnetResourceId = $existingVnetAvdSubnetResourceId 
-#     existingVnetPrivateEndpointSubnetResourceId = $existingVnetPrivateEndpointSubnetResourceId 
-#     identityDomainName = $identityDomainName 
-#     avdOuPath = $avdOuPath
-# }
 
 # if ($update_existing_stack -eq 'true') {
 #     Write-Host "Updating existing stack"
-#     Set-AzSubscriptionDeploymentStack @paramAzSubscriptionDeploymentStackDeployment -P
+#     Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#         -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+#         -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+#         -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
 #     return
 # } else {
 #     Write-Host "Creating new stack"
-#     New-AzSubscriptionDeploymentStack @paramAzSubscriptionDeploymentStackDeployment -P
+#     New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
+#     -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
+#     -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
+#     -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
 #     return
 # }
 
@@ -47,13 +38,13 @@ if ($update_existing_stack -eq 'true') {
     Set-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
         -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
         -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
-        -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
+        -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId -avdHostPoolType $avdHostPoolType -avdDeploySessionHostsCount $avdDeploySessionHostsCount
     return
 } else {
     Write-Host "Creating new stack"
     New-AzSubscriptionDeploymentStack -Name $DeploymentStackName -Location $Location -TemplateFile $TemplateFile -TemplateParameterFile $ParametersFile -P -ActionOnUnmanage "detachAll" -DenySettingsMode "none" `
     -avdSessionHostCustomNamePrefix $avdSessionHostCustomNamePrefix -deploymentEnvironment $deploymentEnvironment -avdWorkloadSubsId $avdWorkloadSubsId -imageGallerySubscriptionId $imageGallerySubscriptionId `
     -existingVnetAvdSubnetResourceId $existingVnetAvdSubnetResourceId -existingVnetPrivateEndpointSubnetResourceId $existingVnetPrivateEndpointSubnetResourceId `
-    -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId
+    -identityDomainName $identityDomainName -avdOuPath $avdOuPath -securityPrincipalId $securityPrincipalId -avdHostPoolType $avdHostPoolType -avdDeploySessionHostsCount $avdDeploySessionHostsCount
     return
 }
